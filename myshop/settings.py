20ba31c94ac89b2c46726430774d4eb43151609e
Georgiy <gogapryst@gmail.com>
2024-11-07 17:35:52 +0300

    add order processing with celery

--- myshop/settings.py
+++ myshop/settings.py
@@ -131,3 +131,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
