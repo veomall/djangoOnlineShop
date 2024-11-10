0a9719003719be10e234a26ade6263324277aba0
Georgiy <gogapryst@gmail.com>
2024-11-10 17:02:31 +0300

    add order view and scv exporting in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 from django.utils.safestring import mark_safe
-
-
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    raw_id_fields = ['product']
+import csv
+import datetime
+from django.http import HttpResponse
+from django.urls import reverse
 
 
 def order_payment(obj):
@@ -15,6 +14,42 @@ def order_payment(obj):
         return mark_safe(html)
     return ''
 order_payment.short_description = 'Stripe payment'
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = (
+        f'attachment; filename={opts.verbose_name}.csv'
+    )
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
 
 
 @admin.register(Order)
@@ -30,7 +65,9 @@ class OrderAdmin(admin.ModelAdmin):
         'paid',
         order_payment,
         'created',
-        'updated'
+        'updated',
+        order_detail,
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from cart.cart import Cart
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from .tasks import order_created
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def order_create(request):
@@ -32,4 +33,11 @@ def order_create(request):
         request,
         'orders/order/create.html',
         {'cart': cart, 'form': form}
+    )
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(
+        request, 'admin/orders/order/detail.html', {'order': order}
     )
